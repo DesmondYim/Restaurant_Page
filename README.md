1f9e220d1639aa4f866bb6c8123d1b56ee08c0da
Desmond Yim <desmond.y01@gmail.com>
2023-11-06 14:17:22 -0700

    Create index.js and rename current to load-page.js

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # Restaurant_Page
+This is an assignment from the Odin Project with the aim to create 
+a restaurant page with npm and webpack, using JS to generate content for the page.
---
+++ src/modules/load-page.js
@@ -0,0 +1,34 @@
+import MainPic from './img/restaurant.jpg';
+import './style.css';
+
+const content = document.querySelector('#content');
+
+function createHeader() {
+    const restaurantHeader = document.createElement('h1');
+    restaurantHeader.id = "restaurantName";
+
+    restaurantHeader.innerHTML = "Yim Fam";
+
+    return restaurantHeader;
+}
+
+function addRestaurantPic() {
+    const restaurantPic = new Image();
+    restaurantPic.classList = "main_pic";
+    restaurantPic.src = MainPic;
+
+    return restaurantPic;
+}
+
+function createDescript() {
+    const restrDescript = document.createElement('div');
+    restrDescript.classList = "description";
+
+    restrDescript.innerHTML = "Welcome to Yim Fam where we serve the best of Mama Yim's homemade cooking!"
+
+    return restrDescript;
+}
+
+content.appendChild(createHeader());
+content.appendChild(addRestaurantPic());
+content.appendChild(createDescript());
