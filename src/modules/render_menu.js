8b7d1b197595ed62f844df73051f7f1b57a817ad
Desmond Yim <desmond.y01@gmail.com>
2023-11-14 15:38:27 -0700

    Add menu listed items

--- src/modules/render_menu.js
+++ src/modules/render_menu.js
@@ -1,3 +1,8 @@
+import TiggerTails from '../img/food1.jpg';
+import PoohTarts from '../img/food2.jpg';
+import AcreSandwiches from '../img/food3.jpg';
+import PoohPuffs from '../img/food4.png';
+
 function createItemImage(imageFile) {
     const image = new Image();
     image.src = imageFile;
@@ -13,7 +18,6 @@ function createParagraph(text) {
 
 function createItemDetail(itemName, cost, detail) {
     const summary = document.createElement('div');
-    summary.classList = 'itemSummary';
 
     const itemName = createParagraph(itemName);
     const itemCost = createParagraph(`$`+cost);
@@ -30,7 +34,9 @@ function createItem(imageFile, itemName, cost, detail) {
     menuItem.classList = 'menuItem';
 
     const image = createItemImage(imageFile);
+    image.classList = 'menuItemImage';
     const details = createItemDetail(itemName, cost, detail);
+    details.classList = 'menuItemDetail';
 
     menuItem.appendChild(image);
     menuItem.appendChild(details);
@@ -38,7 +44,24 @@ function createItem(imageFile, itemName, cost, detail) {
 }
 
 function renderMenu() {
-    
+    const mainBody = document.createElement('div');
+    mainBody.classList = 'mainBody';
+
+    const food1 = createItem(TiggerTails, `Tigger Tails`, 5, `Chocolate \
+        and honey covered pretzels`);
+    const food2 = createItem(PoohTarts, `Pooh Tarts`, 4, `Adorable egg tarts \
+        with an assortment of fruit flavors`);
+    const food3 = createItem(AcreSandwiches, `Acre Sandwiches`, 7, `Sandwiches \
+        with an smackerel of honey ham and cheese slices`);
+    const food4 = createItem(PoohPuffs, `Pooh Puffs`, 2, `Sweet and refreshing \
+        cream puffs in the shape of your favourite Winnie the Pooh`);
+
+    mainBody.appendChild(food1);
+    mainBody.appendChild(food2);
+    mainBody.appendChild(food3);
+    mainBody.appendChild(food4);
+
+    return mainBody;
 }
 
 export default renderMenu;
